d4e796e1ecbe97402b4fdada3dfd3293b0828062
highpitchit <ameliafrancesnunn@gmail.com>
2020-07-07 15:51:39 +1000

    configuring express

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import Bookmarks from './Bookmarks'
 import NoMatch from './NoMatch'
 import Navbar from './Navbar'
+import {stateReducer, StateContext} from './store'
 
 const App = () => {
+  const [state, dispatch] = useReducer(stateReducer, { bookmarks: [] })
+
   return (
-    <>
+    <StateContext.Provider value= {{ state, dispatch }}>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/bookmarks" component={Bookmarks} />
         <Route component={NoMatch} />
       </Switch>
-    </>
+    </StateContext.Provider>
   )
 }
 
--- src/Bookmarks.jsx
+++ src/Bookmarks.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StateContext } from './store'
 
 export default function Bookmarks() {
+    const {state, dispatch} = useContext(StateContext)
+
     return (
         <div>
             <h1>Bookmarks</h1>
